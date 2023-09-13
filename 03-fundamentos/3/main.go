12349b3d656d00ce4ca8a8da5170870d276df769
jeferson.sousa <jeferson.sousa@redevistorias.com.br>
2023-09-13 20:38:11 -0300

    start

---
+++ 03-fundamentos/3/main.go
@@ -0,0 +1,23 @@
+package main
+
+const a = "Hello, World!"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Jeferson"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X"
+
+	println(a)
+	//println(b)
+	// println(c)
+	// println(d)
+	// println(e)
+}
+func x() {
+
+}
