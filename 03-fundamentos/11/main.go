dff97a98fca9353f5ed7076d7974665e603ee74d
jeferson.sousa <jeferson.sousa@redevistorias.com.br>
2023-10-17 19:20:22 -0300

    struct

--- 03-fundamentos/11/main.go
+++ 03-fundamentos/11/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+}
+
 func main() {
+	jeferson := Cliente{
+		Nome:  "Jeferson",
+		Idade: 29,
+		Ativo: true,
+	}
+	jeferson.Ativo = false
+
+	fmt.Printf("Nome: %s, Idade: %d, Ativo: %t\n", jeferson.Nome, jeferson.Idade, jeferson.Ativo)
 
 }
