8931ecdb0ca728c864ba443d8e1826df25de631d
jeferson.sousa <jeferson.sousa@redevistorias.com.br>
2023-10-18 20:09:54 -0300

    fundamentos

---
+++ 03-fundamentos/15/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+	a := 10
+	var ponteiro *int = &a
+	println(a)
+	println(&a)
+	println(ponteiro)
+	*ponteiro = 20
+	println(a)
+	b := &a
+	println(b)
+	println(*b)
+	*b = 30
+	println(a)
+}
